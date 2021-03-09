f9e84550dc84759a0ce8d7642272c6491ebafb94
Jason <kangja1996@gmail.com>
2021-03-09 16:20:20 -0500

    fixed the error

--- snapchat-clone/src/Chat.js
+++ snapchat-clone/src/Chat.js
@@ -2,6 +2,7 @@ import { Avatar } from "@material-ui/core";
 import React from "react";
 import "./Chat.css";
 import StopRoundedIcon from "@material-ui/icons/StopRounded";
+import TimeAgo from "timeago-react";
 
 function Chat({ id, username, timestamp, read, imageUrl, profilePic }) {
   return (
@@ -9,7 +10,10 @@ function Chat({ id, username, timestamp, read, imageUrl, profilePic }) {
       <Avatar className="chat__avatar" src={profilePic} />
       <div className="chat__info">
         <h4>{username}</h4>
-        <p>Tap to view - {new Date(timestamp?.toDate()).toUTCString()} </p>
+        <p>
+          Tap to view -{" "}
+          <TimeAgo date={new Date(timestamp?.toDate()).toUTCString()} />
+        </p>
       </div>
 
       {!read && <StopRoundedIcon className="chat__readIcon" />}
